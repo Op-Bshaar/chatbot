b7ad1593eb4982d9f3b3f8b52d281d9b2cef605b
Op_Bashar <basharabdalla5@gmail.com>
2024-04-20 10:58:39 +0200

    ..

--- ai_chatbot/chatbot/views.py
+++ ai_chatbot/chatbot/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 def chatbot(request):
-    return HttpResponse("Hello world!")
+     template = loader.get_template('chatbot.html')
+     return HttpResponse(template.render({},request))
